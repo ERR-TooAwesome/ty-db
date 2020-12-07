f9c57d14195a6e9845c89b401eb779b770829069
Tyler Duncan <tylerduncan365@yahoo.com>
2020-12-07 13:10:00 -0600

    Mocked up server and added basic template for routes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const queryProductInfo = require('./routes/queryProductInfo.js');
+const queryProductStyle = require('./routes/queryProductStyle.js');
+const queryProductList = require('./routes/queryProductList.js');
 
+app.use(express.json());
+
+//test
 app.get('/', (req, res) => {
   res.send('Test working');
+})
+
+//product Information GET request
+app.get('/products/:product_id', (req, res) => {
+  //use queryProductInfo route
+  res.status(200).send('product info')
+})
+
+//product Styles GET Request
+app.get('/products/:product_id/styles', (req, res) => {
+  //use queryProductStyle route
+  res.status(200).send('product styles')
+})
+
+app.get('/products/list', (req, res) => {
+  //use queryProductList route
+  res.status(200).send('product list')
 })
 
 app.listen(port, () => {
